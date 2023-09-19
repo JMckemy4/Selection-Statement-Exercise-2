44cb51633811092869eec6cfadc21bfcd7b82459
jared mckemy <jaredmkcemy4@gmail.com>
2023-09-19 12:54:14 -0700

    finished exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System;
+
+namespace SelectionStatementsExercise2
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string userSubject = Console.ReadLine();
+
+            switch (userSubject.ToLower()) // Convert user input to lowercase for case-insensitivity
+            {
+                case "math":
+                    Console.WriteLine($"Your favorite subject is {userSubject}. Math is fascinating!");
+                    break;
+                case "science":
+                    Console.WriteLine($"Your favorite subject is {userSubject}. Science is full of wonders.");
+                    break;
+                case "history":
+                    Console.WriteLine($"Your favorite subject is {userSubject}. History helps us learn from the past.");
+                    break;
+                case "english":
+                    Console.WriteLine($"Your favorite subject is {userSubject}. English language and literature are enriching.");
+                    break;
+                case "art":
+                    Console.WriteLine($"Your favorite subject is {userSubject}. Art allows for creative expression.");
+                    break;
+                default:
+                    Console.WriteLine($"I'm not familiar with {userSubject}. Please enter a valid school subject.");
+                    break;
+            }
         }
     }
 }
+
+
+
+
+
+
